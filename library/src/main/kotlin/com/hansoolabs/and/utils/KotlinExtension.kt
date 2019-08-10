3c406130a2448e8a94a374e96bc9e0f36c0b79b1
Hyonsoo Han <hansune@me.com>
2019-08-10 10:13:14 +0900

    added Internet available func

--- library/src/main/kotlin/com/hansoolabs/and/utils/KotlinExtension.kt
+++ library/src/main/kotlin/com/hansoolabs/and/utils/KotlinExtension.kt
@@ -3,6 +3,8 @@ package com.hansoolabs.and.utils
 import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.fragment.app.Fragment
 import android.view.View
 import com.google.android.material.textfield.TextInputLayout
@@ -10,7 +12,6 @@ import com.google.android.material.textfield.TextInputLayout
 /**
  * Created by brownsoo on 2017. 8. 3..
  */
-
 
 fun Activity.isLive() = !this.isFinishing && !this.isDestroyed
 
@@ -24,6 +25,35 @@ fun View.setVisible(visible: Boolean) {
     } else {
         View.GONE
     }
+}
+
+@Suppress("DEPRECATION")
+fun Context.isInternetAvailable(): Boolean {
+    var result = false
+    val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        cm?.run {
+            cm.getNetworkCapabilities(cm.activeNetwork)?.run {
+                result = when {
+                    hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            }
+        }
+    } else {
+        cm?.run {
+            cm.activeNetworkInfo?.run {
+                if (type == ConnectivityManager.TYPE_WIFI) {
+                    result = true
+                } else if (type == ConnectivityManager.TYPE_MOBILE) {
+                    result = true
+                }
+            }
+        }
+    }
+    return result
 }
 
 fun Context.versionName(): String {
