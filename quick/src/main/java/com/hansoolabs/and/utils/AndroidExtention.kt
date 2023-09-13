076d9a9a85610f0efbf8329cbe74e54d22798282
brownsoo <hansune@me.com>
2023-09-13 10:09:51 +0900

    [feat] getParcelableArrayListExtraCompat

--- quick/src/main/java/com/hansoolabs/and/utils/AndroidExtention.kt
+++ quick/src/main/java/com/hansoolabs/and/utils/AndroidExtention.kt
@@ -15,6 +15,15 @@ fun Uri.isFileUri(): Boolean {
     return scheme == "file"
 }
 
+fun<T: Parcelable> Intent.getParcelableArrayListExtraCompat(key: String, kl: Class<T>): ArrayList<T>? {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        return this.getParcelableArrayListExtra<T>(key)
+    } else {
+        return this.getParcelableArrayListExtra(key, kl)
+    }
+}
+
 fun<T: java.io.Serializable> Intent.getSerializableExtraCompat(key: String, kl: Class<T>): T? {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
         @Suppress("UNCHECKED_CAST", "DEPRECATION")
