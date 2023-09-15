1e1b39cf50f152631a91ac2debaf0b1abafd8bec
brownsoo <hansune@me.com>
2023-09-15 12:38:44 +0900

    Update AndroidExtention.kt

--- quick/src/main/java/com/hansoolabs/and/utils/AndroidExtention.kt
+++ quick/src/main/java/com/hansoolabs/and/utils/AndroidExtention.kt
@@ -17,10 +17,19 @@ fun Uri.isFileUri(): Boolean {
 
 fun<T: Parcelable> Intent.getParcelableArrayListExtraCompat(key: String, kl: Class<T>): ArrayList<T>? {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        @Suppress("DEPRECATION")
         return this.getParcelableArrayListExtra<T>(key)
     } else {
         return this.getParcelableArrayListExtra(key, kl)
+    }
+}
+
+fun<T: Parcelable> Intent.getParcelableArrayExtraCompat(key: String, kl: Class<T>): Array<T>? {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        return this.getParcelableArrayExtra(key) as? Array<T>
+    } else {
+        return this.getParcelableArrayExtra(key, kl)
     }
 }
 
@@ -34,10 +43,31 @@ fun<T: java.io.Serializable> Intent.getSerializableExtraCompat(key: String, kl: 
 
 fun<T: Parcelable> Intent.getParcelableExtraCompat(key: String, kl: Class<T>): T? {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        @Suppress("DEPRECATION")
         return this.getParcelableExtra(key) as T?
     }
     return this.getParcelableExtra(key, kl)
+}
+
+// Bundle
+
+
+fun<T: Parcelable> Bundle.getParcelableArrayListCompat(key: String, kl: Class<T>): ArrayList<T>? {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("DEPRECATION")
+        return this.getParcelableArrayList<T>(key)
+    } else {
+        return this.getParcelableArrayList(key, kl)
+    }
+}
+
+fun<T: Parcelable> Bundle.getParcelableArrayCompat(key: String, kl: Class<T>): Array<T>? {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        return this.getParcelableArray(key) as? Array<T>
+    } else {
+        return this.getParcelableArray(key, kl)
+    }
 }
 
 fun<T: java.io.Serializable> Bundle.getSerializableCompat(key: String, kl: Class<T>): T? {
@@ -50,7 +80,7 @@ fun<T: java.io.Serializable> Bundle.getSerializableCompat(key: String, kl: Class
 
 fun<T: Parcelable> Bundle.getParcelableCompat(key: String, kl: Class<T>): T? {
     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-        @Suppress("UNCHECKED_CAST", "DEPRECATION")
+        @Suppress("DEPRECATION")
         return this.getParcelable(key) as T?
     }
     return this.getParcelable(key, kl)
